6cbf7dd3c35bfcb136a6d83ed41b7cb20e4e6d6b
saabra926 <rasaab926@gmail.com>
2025-07-31 01:51:03 -0700

    mean abi kafi kuch add kiya ha
    kafi kuch kkiya ha add

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Game from './Game';
+import WhopCallback from './pages/whop/callback';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Game />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Game />} />
+        <Route path="/whop/callback" element={<WhopCallback />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
