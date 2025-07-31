19ee48ba019287c412cabdfa42ff4915690f93b3
saabra926 <rasaab926@gmail.com>
2025-07-31 02:47:33 -0700

    ye b kuch new cahnge hua ha
    kuch new chaneg in my website

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Game from './Game';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Game />} />
-      </Routes>
-    </BrowserRouter>
+    <Game />
   </React.StrictMode>
 );
